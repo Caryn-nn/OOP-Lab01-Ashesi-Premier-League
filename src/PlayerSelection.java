f598110b2ff8473c1a87670f79f56cc5b935e38b
caryn <caryn.awittor@ashesi.edu.gh>
2025-09-22 09:09:57 +0000

    Added a description of what my entire code will be doing

--- src/PlayerSelection.java
+++ src/PlayerSelection.java
@@ -1,3 +1,8 @@
+// Author: Fafali Caryn Aku Awittor
+// Date: 9th September, 2025
+// Description: Building a complete program to help coach of the Ashesi Premier League make decision about the player eligibility,position and lineup
+
+
 public class PlayerSelection {
     public static void main(String[] args) {
 
