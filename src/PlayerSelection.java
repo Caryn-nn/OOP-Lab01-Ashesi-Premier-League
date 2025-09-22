12aa59f40d49043fb4c3162f3b533c4b4d37d9e7
caryn <caryn.awittor@ashesi.edu.gh>
2025-09-22 09:11:25 +0000

    Importing the scanner package to allow input

--- src/PlayerSelection.java
+++ src/PlayerSelection.java
@@ -2,7 +2,8 @@
 // Date: 9th September, 2025
 // Description: Building a complete program to help coach of the Ashesi Premier League make decision about the player eligibility,position and lineup
 
-
+// importing the scanner package to allow input
+import java.util.Scanner;
 public class PlayerSelection {
     public static void main(String[] args) {
 
