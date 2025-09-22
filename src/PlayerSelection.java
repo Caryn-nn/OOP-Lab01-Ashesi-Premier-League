80a24348cc529fe7ed4e956f366f1cf247114015
caryn <caryn.awittor@ashesi.edu.gh>
2025-09-22 14:15:32 +0000

    editting my code to meet requirements

--- src/PlayerSelection.java
+++ src/PlayerSelection.java
@@ -1,7 +1,7 @@
 // Author: Fafali Caryn Aku Awittor
 // Instructor : Daniel Addo
 // Class : OOP Lab 1 (Cohort A)
-// Date: 9th September, 2025
+// Date: 22th September, 2025
 // Description: Building a complete program to help coach of the Ashesi Premier League make decision about the player eligibility,position and lineup
 
 // importing the scanner package to allow input
